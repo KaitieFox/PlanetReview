3e0c64f4e286a0e8e1d118f9fda8d204c29fb1ba
KaitieFox <kaitiefox@gmail.com>
2017-10-27 16:04:09 -0400

    Added Models

--- PlanetReview/Models/Category.cs
+++ PlanetReview/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,13 @@ namespace PlanetReview.Models
 {
     public class Category
     {
+        [Key]
+        public int ID { get; set; }
+        [Display(Name = "Planet Name")]
+        public string PlanetName { get; set; }
+        [Display(Name = "Planet Type")]
+        public string PlanetType { get; set; }
+
+        public virtual ICollection<Review> Review { get; set; }
     }
 }
--- PlanetReview/Models/Review.cs
+++ PlanetReview/Models/Review.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,18 @@ namespace PlanetReview.Models
 {
     public class Review
     {
+        [Key]
+        public int ID { get; set; }
+        [Display(Name = "Review Summary"), MaxLength(50)]
+        public string Title { get; set; }
+        [MaxLength(500)]
+        public string Content { get; set; }
+        public DateTime PublishDate { get; set; }
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+        [ForeignKey("Category") Display(Name = "Planets")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
     }
 }
